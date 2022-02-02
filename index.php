a6dba4b0a91bbca7f05421adb46ae9b69b9b637c
Alessandro <alessandrocosta103@gmail.com>
2022-02-02 18:13:05 -0300

    altera Login

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Farmacia</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link href="estilo/estiloLogin.css" rel="stylesheet" type="text/css">
     </head>
+
     <header>
-        <center><h1> LOGIN </h1></center>
+
     </header>
+
     <body>
-        <div login>
-    <center>
-    <form action="menu.php" method="post">
-    <input type="text" name="user" id="user" placeholder="USUARIO"><p>
-    <input type="password" name="senha" id="senha" placeholder="SENHA"><p>
-    <input type="submit" name="botao" id="botao">
-        </form>
-    </center>
+        <div class="container">
+            <div class="text-center">
+                <div class="form-control">
+                    <div class="mb-3">
+                        <form action="menu.php" method="post">
+                            <h2 class="text-center"> LOGIN </h2>
+                            <input type="text" class="text-center" name="user" id="user" placeholder="USUÁRIO"><p>
+                            <input type="password" class="text-center" name="senha" id="senha" placeholder="SENHA"><p>
+                                <button type="submit" class="btn btn-primary">Enviar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
+
+
     <footer>
-       <center><h3> FARMACIA BREVES </h3></center> 
+
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+    crossorigin="anonymous"></script>
 </html>
 
