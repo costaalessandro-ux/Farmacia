f0394b265916872f353b674f9c55ae429c26cd1a
Alessandro <alessandrocosta103@gmail.com>
2022-03-22 18:12:43 -0300

    altera conexao

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO('pgsql:host=ec2-44-195-191-252.compute-1.amazonaws.com;
+    $conn = new PDO('pgsql:host=heroku-farmacia;
   dbname=d3hhqau0sdm9ps',"adlwwijslzhomn","0283346bb0370dfd1bb2ca2ed67d4f0fefefa6087418ed85ede0ca4793ad6f9a
 ");
     $conn->setAttribute(PDO::ATTR_ERRMODE,
